e7d863daabf988d71509e1c8eb4a84bea253e109
J.M de la Rosa Trevin <delarosatrevin@gmail.com>
2026-08-07 09:18:02 -0500

    Change default cores per gpu based on queue or gpu_type

--- emwrap/base/project_manager.py
+++ emwrap/base/project_manager.py
@@ -555,21 +555,25 @@ class ProjectManager(FolderManager):
 
         cpus = _load_cpus(gpus)
 
-        if gpus > 0:
-            cpus = max(cpus, gpus * 10)
+        gpu_type = qparams.get('gpu_type', 'any')
 
-        if cpus == 0:
-            raise Exception("Neither CPUs nor GPUs are set. Please set at least one of them.")
+        if gpus > 0:
+            # FIXME: Move all this cluster dependent logic to a separate function
+            # that could be configured by a environment variable or a config file.            
+            cores_per_gpu = {'cryo_core': 12, 'gpu_rtx': 30}.get(qname, {'A100': 12}.get(gpu_type, 10))
+            cpus = max(cpus, gpus * cores_per_gpu)
 
         if gpus:
             # FIXME: Use emgoat for a more general interaction with HPC
             mig = ':mig=2' if qparams.get('mig', False) else ''
             gpu_line = f'#BSUB -gpu "num={gpus}/host:mode=shared{mig}"'
-            gpu_type = qparams.get('gpu_type', 'any')
             if gpu_type != 'any':
                 gpu_line += f'\n#BSUB -R {gpu_type.lower()}'
         else:
             gpu_line = ''
+
+        if cpus == 0:
+            raise Exception("Neither CPUs nor GPUs are set. Please set at least one of them.")
 
         qparams.update({
             'queue_name': qname,
