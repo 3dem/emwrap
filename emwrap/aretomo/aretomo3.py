8010b456d9cf1b7adf4d8a0e5a19ccc9dbc724f3
Marchán <dmarchn@splprhpc11.cm.cluster>
2026-08-12 10:59:09 -0500

    fix aretomo3 eer upsampling and dose per frame

--- emwrap/aretomo/aretomo3.py
+++ emwrap/aretomo/aretomo3.py
@@ -79,8 +79,8 @@ class AreTomo3:
         })
         if gain := acq.get('gain', None):
             args['-Gain'] = gain
-        if dose := acq.get('total_dose', None):
-            args['-FmDose'] = dose
+        # if dose := acq.get('total_dose', None):
+            # args['-FmDose'] = dose
 
         return args
     
