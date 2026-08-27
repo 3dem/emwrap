ae08bc56f07073a518bd827a84cdc464f0f65e1a
Marchán <dmarchn@splprhpc12.cm.cluster>
2026-08-27 15:33:14 -0500

    add aretomo3 to the config

--- emwrap/base/config.py
+++ emwrap/base/config.py
@@ -119,6 +119,21 @@ class ProcessingConfig:
             "output": "Refine3D",
             "tomo": True
         },
+        "emw-aretomo3": { 
+            "launcher": "$SCRIPTS/emwrap_launcher.sh emwrap.aretomo.aretomo3_pipeline",
+            "label": "Aretomo3",
+            "output": "Aretomo3"
+        },
+        "emw-aretomo3-align": {
+            "launcher": "$SCRIPTS/emwrap_launcher.sh emwrap.aretomo.aretomo3_align",
+            "label": "Aretomo3 Align",
+            "output": "Aretomo3Align"
+        },
+        "emw-aretomo3-reconstruct": {
+            "launcher": "$SCRIPTS/emwrap_launcher.sh emwrap.aretomo.aretomo3_reconstruct",
+            "label": "Aretomo3 Reconstruct",
+            "output": "Aretomo3Reconstruct"
+        },
         "emw-denoiset": {
             "launcher": "emwrap.aretomo.denoiset",
             "label": "DenoisET",
@@ -132,7 +147,7 @@ class ProcessingConfig:
         { "name": "warp", "prefixes": ["emw-warp"] },
         { "name": "relion", "prefixes": ["emw-relion", "relion."] },
         { "name": "pytom", "prefixes": ["emw-pytom"] },
-        { "name": "aretomo", "prefixes": ["emw-aretomo", "emw-denoiset"] }
+        { "name": "aretomo", "prefixes": ["emw-aretomo", "emw-aretomo3", "emw-aretomo3-align", "emw-aretomo3-reconstruct", "emw-denoiset"] }
     ]
 
     @classmethod
